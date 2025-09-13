57d414a2c3e15376912662989d26c1dfb7f931f1
syarotokki <maikuratokki@gmail.com>
2025-09-14 04:30:44 +0900

    server.py

--- server.py
+++ server.py
@@ -25,4 +25,6 @@ def update_orders():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
